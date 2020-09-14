d24a23469f0458f8210267e0abcd4ce30db6f2ce
Ksenofontov Boris <xenofontb@gmail.com>
2020-09-14 12:17:38 +0900

    first commit

---
+++ ТРПО №1/ТРПО №1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ТРПО__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int B,A,C;
+            Console.WriteLine("Введите a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите c:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            A = a + b;
+            B = c - a;
+            C = a + b + c;
+            b = B;
+            c = C;
+            a = A;
+            Console.WriteLine(a+","+b+","+c);
+        }
+    }
+}
